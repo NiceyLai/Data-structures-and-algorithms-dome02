b09dc6545fad856f7adf64d5e81cdb41f5f0b218
NiceyLai <laisi1127@163.com>
2022-08-21 12:10:40 +0800

    quickSort

---
+++ quickSort.js
@@ -0,0 +1,21 @@
+// 快速排序
+
+let quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let pivotIndex = Math.floor(arr.length / 2);
+  let pivot = arr.splice(pivotIndex, 1)[0];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
+};
+let numbers = [9, 4, 23, 7];
+console.log(quickSort(numbers));
